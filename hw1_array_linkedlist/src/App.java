d37c067e560d7baa92c0e4a781cfa8a8e143fdf1
carrie57 <carrie91507@gmail.com>
2025-10-02 13:29:12 -0700

    refactor: write hw1 in wrong folder which is hw2

--- hw1_array_linkedlist/src/App.java
+++ hw1_array_linkedlist/src/App.java
@@ -1,5 +1,15 @@
+import bank.Account;
+import bank.MyLinkedList;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Running hw1_array_linkedlist ...");
+        MyLinkedList list = new MyLinkedList();
+        list.addSorted(new Account(3, "Joe", "4201 pv rd", "1234567", 0));
+        list.addSorted(new Account(2, "Amy", "4202 pv rd", "1237654", 200));
+        list.addSorted(new Account(7, "Bob", "4203 pv rd", "1239876", 30.56));
+        list.addSorted(new Account(5, "Sam", "4204 pv rd", "1230098", 100.7));
+
+        list.printList();
+        
     }
 }
--- hw2_stack_queue/src/App.java
+++ hw2_stack_queue/src/App.java
@@ -3,13 +3,7 @@ import bank.Account;
 
 public class App {
     public static void main(String[] arg){
-        MyLinkedList list = new MyLinkedList();
-        list.addSorted(new Account(3, "Joe", "4201 pv rd", "1234567", 0));
-        list.addSorted(new Account(2, "Amy", "4202 pv rd", "1237654", 200));
-        list.addSorted(new Account(7, "Bob", "4203 pv rd", "1239876", 30.56));
-        list.addSorted(new Account(5, "Sam", "4204 pv rd", "1230098", 100.7));
-
-        list.printList();
+        System.out.println("Running hw1_array_linkedlist ...");
 
     }
 }
