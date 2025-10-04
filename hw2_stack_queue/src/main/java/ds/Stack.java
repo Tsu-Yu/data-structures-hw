b6b87459976ed47281d21e7c27ead13242b9d4c3
carrie57 <carrie91507@gmail.com>
2025-10-04 13:45:15 -0700

    feat: task1 stack basic function design

---
+++ hw2_stack_queue/src/main/java/ds/Stack.java
@@ -0,0 +1,18 @@
+package main.java.ds;
+
+public class Stack<E> {
+    public void push (E item) {
+    }
+    public E pop() {
+        return null;
+    }
+    public E peek() {
+        return null;
+    }
+    public int size() {
+        return 0;
+    }
+    public boolean isEmpty() {
+        return true;
+    }
+}
