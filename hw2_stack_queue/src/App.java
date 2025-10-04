0391ce8b2818193d15031eeedd922bb78d400552
carrie57 <carrie91507@gmail.com>
2025-10-04 13:44:05 -0700

    chore: initialize hw2 folder structure with Maven rules

--- hw2_stack_queue/src/App.java
+++
@@ -1,9 +0,0 @@
-import bank.MyLinkedList;
-import bank.Account;
-
-public class App {
-    public static void main(String[] arg){
-        System.out.println("Running hw1_array_linkedlist ...");
-
-    }
-}
---
+++ hw2_stack_queue/src/test/java/ds/StackTest.java
@@ -0,0 +1,5 @@
+package test.java.ds;
+
+public class StackTest {
+    
+}
